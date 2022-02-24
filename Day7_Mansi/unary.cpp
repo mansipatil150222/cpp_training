c5da91e93825ad1bbca60abf528f0c4bbd036725
Mansi Patil <98096141+mansipatil150222@users.noreply.github.com>
2022-02-24 11:28:32 +0530

    Update unary.cpp

--- Day7_Mansi/unary.cpp
+++ Day7_Mansi/unary.cpp
@@ -13,19 +13,29 @@ class Sample{
         void display(){
             cout<<"a = "<<a<<","<<"b = "<<b<<endl;
         }
-        friend Sample operator--(Sample passed){ //friend function
+        friend Sample operator--(Sample passed){ //friend function for decrement 
             Sample temp;
             temp.a = --passed.a;
             temp.b = --passed.b;
             return temp;
         }
+        friend Sample operator++(Sample passed){ //friend function for increment
+            Sample temp;
+            temp.a = ++passed.a;
+            temp.b = ++passed.b;
+            return temp;
+        }
 }; //end of class
 int main(){
 
-    Sample s1(10,20), s2, s3(100,100); //object creation of sample class
-    s1.display(); //function calling using object
-
+    Sample s2, s3(100,100); //object creation of sample class
+    s3.display(); //function calling using object
+    cout<<"after performing operation:"<<endl;
+    cout<<"decrement operation:"<<endl;
     s2 = --s3; // overloading operator and stroring result in another object.
+    s2.display(); //function calling using object
+    cout<<"increment operation:"<<endl;
+    s2 = operator++(s3);// overloading operator and stroring result in another object.
     s2.display(); //function calling using object
 
     return 0;
